02cba37c4bfaf7fa5dbc51cd4403930c76aff9d5
JingZz7 <1950662032@qq.com>
2023-03-11 22:19:34 +0800

    feat: Initialize the relevant files

---
+++ commons/src/main/java/com/zhiyixingnan/config/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.zhiyixingnan.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author: ZJ @Date: 2022-11-05 10:31:03 10:31 @Description: CORS 配置允许前端跨域访问
+ */
+@Configuration
+public class CorsConfig {
+  private CorsConfiguration buildConfig() {
+    CorsConfiguration corsConfiguration = new CorsConfiguration();
+    /*是否允许请求带有验证信息*/
+    corsConfiguration.setAllowCredentials(true);
+    /*允许访问的客户端域名*/
+    corsConfiguration.addAllowedOrigin("*"); // 1
+    /*允许服务端访问的客户端请求头*/
+    corsConfiguration.addAllowedHeader("*"); // 2
+    /*允许访问的方法名,GET POST等*/
+    corsConfiguration.addAllowedMethod("*"); // 3
+    return corsConfiguration;
+  }
+
+  @Bean
+  public CorsFilter corsFilter() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", buildConfig()); // 4
+    return new CorsFilter(source);
+  }
+}
---
+++ commons/src/main/java/com/zhiyixingnan/config/MPConfig.java
@@ -0,0 +1,25 @@
+package com.zhiyixingnan.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MPConfig {
+  /**
+   * @param
+   * @return com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor
+   * @author ZJ
+   * @description //TODO MyBatisPlus拦截器
+   * @date 2022-10-26 13:45:26 13:45
+   */
+  @Bean
+  public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+    interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+    interceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+    return interceptor;
+  }
+}
