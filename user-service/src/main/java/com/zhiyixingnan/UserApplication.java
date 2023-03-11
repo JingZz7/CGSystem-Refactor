a39e78d4b3d9b8dcd58a693eabb7f728abfab430
JingZz7 <1950662032@qq.com>
2023-03-11 18:49:46 +0800

    feat: add seata etc.

--- user-service/src/main/java/com/zhiyixingnan/UserApplication.java
+++ user-service/src/main/java/com/zhiyixingnan/UserApplication.java
@@ -1,10 +1,12 @@
 package com.zhiyixingnan;
 
+import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableAutoDataSourceProxy
 @EnableFeignClients
 public class UserApplication {
   public static void main(String[] args) {
